7be6ccebd75faef9895dd6048a6140ce29fbd1c0
Meri <meri.calvo@flywire.com>
2017-09-08 10:38:44 +0200

    Add tennis kata description

--- katas/tennis/web/kata_content.js
+++ katas/tennis/web/kata_content.js
@@ -1,11 +1,20 @@
 const kata = {
-  description: `<p></p>`,
+  description: `<p>
+                  This Kata is about implementing a simple tennis game. It is based on Wii tennis, where they have simplified tennis, so each set is one game.
+                </p>`,
 
-  yellowBelt: `<p></p>`,
+  yellowBelt: `<p>
+                  The scoring system is rather simple:
+                  <ol>
+                    <li>Each player can have either of these points in one game 0 15 30 40</li>
+                    <li>If you have 40 and you win the ball you win the game, however there are special rules.</li>
+                    <li>If both have 40 the players are deuce. a. If the game is in deuce, the winner of a ball will have advantage and game ball. b. If the player with advantage wins the ball he wins the game c. If the player without advantage wins they are back at deuce.</li>
+                  </ol>
+                </p>`,
 
-  greenBelt: `<p></p>`,
+  greenBelt: `<p>TBD...</p>`,
 
-  redBelt: `<p></p>`,
+  redBelt: `<p>TBD...</p>`,
 
-  blackBelt: `<p></p>`
+  blackBelt: `<p>TBD...</p>`
 }
